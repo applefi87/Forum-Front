eae15a17749228272cd2aa83267704a1539351a6
applefi87 <werrtyyui@yahoo.com.tw>
2022-07-21 21:20:39 +0800

    Quasar init + I18n

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px">
+    <p>{{ $t('success') }}</p>
+  </q-page>
+</template>
+
+<script setup>
+import { defineComponent } from 'vue'
+
+defineComponent({
+  name: 'IndexPage'
+})
+</script>
