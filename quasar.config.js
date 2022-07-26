5bbd4f4d6186bef613b56a7e25ed20d92864dfff
School PC <werrtyyui@yahoo.com.tw>
2022-07-26 13:23:20 +0800

    t

--- quasar.config.js
+++ quasar.config.js
@@ -54,7 +54,6 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
-
       extendViteConf (viteConf) {
         viteConf.base = ''
       },
