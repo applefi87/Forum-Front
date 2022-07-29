f1d20c7594e467854925d3c5f8a5850a1ddf7758
School PC <werrtyyui@yahoo.com.tw>
2022-07-29 16:40:49 +0800

    t

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,7 +3,6 @@
     <p>{{ text }}</p>
     <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px">
     <p>{{ $t('message') }}</p>
-    <p>{{ process.env.GOOGLE_SHEET }}</p>
     <QuillEditor class="editor" toolbar="full" theme="bubble" v-model:content="text" contentType="html" />
     <!-- <button @click="send">send</button> -->
   </q-page>
@@ -11,40 +10,11 @@
 
 <script setup>
 
-import { defineComponent, ref } from 'vue'
+import { ref } from 'vue'
 import { QuillEditor } from '@vueup/vue-quill'
 // 造型
 import '@vueup/vue-quill/dist/vue-quill.bubble.css'
-// import nodemailer from 'nodemailer'
-// import sendMail from '../assets/sendMail'
-
-// const send = () => {
-//   console.log(process.env.Gmail_PWD)
-// }
-
-// const transporter = nodemailer.createTransport({
-//   host: 'smtp.gmail.com',
-//   port: 465,
-//   auth: {
-//     user: 'applefi87@gmail.com',
-//     pass: 'process.env.Gmail_PWD'
-//   }
-// })
-
-// transporter.sendMail({
-//   from: 'applefi87@gmail.com',
-//   to: 'werrtyyui@yahoo.com.tw',
-//   subject: '測試喔',
-//   html: 'wwww'
-// }).then(info => {
-//   console.log({ info })
-// }).catch(console.error)
-const text = ref('')
-defineComponent({
-  name: 'IndexPage',
-  components: QuillEditor
-}
-)
+const text = console.log(this)
 
 </script>
 
