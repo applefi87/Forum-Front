17fa496bcb80a3612a34cb1b936e145b86e4321b
School PC <werrtyyui@yahoo.com.tw>
2022-08-02 16:33:52 +0800

    處理驗證email

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template >
-  <q-layout view="hHh LpR fff">
+  <q-layout view="hHh LpR fff" id="m">
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
         <q-btn class="lt-md" dense flat round icon="menu" @click="toggleLeftDrawer" />
@@ -16,17 +16,17 @@
           <q-btn-dropdown class="login" dense flat icon="login" @click="login" :label='t("login")' dropdown-icon="none">
             <div class="row no-wrap q-pa-md">
               <div class="column">
-                <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-xs">
+                <q-form @submit="onSubmit" class="q-gutter-xs">
                   <q-input filled v-model="loginForm.account" :label='t("account")' lazy-rules
                     :rules="[val => val && val.length > 0 || t('cantNull')]" />
                   <q-input filled v-model="loginForm.password" :label='t("password")' lazy-rules
                     :rules="[val => val && val.length > 0 || t('cantNull')]" />
                   <div>
                     <div>
-                      <q-checkbox v-model="loginForm.keepLogin" :label='t("keepLogin")' />
+                      <q-checkbox v-model="loginForm.keepLogin" :label='t("keepLogin")' size="xs" color="green" />
                     </div>
                     <q-btn :label='t("login")' type="submit" color="primary" />
-                    <q-btn :label='t("register")' type="reset" color="primary" flat class="q-ml-sm" />
+                    <q-btn :label='t("register")' color="primary" flat class="q-ml-sm" @click="register = true" />
                   </div>
                 </q-form>
               </div>
@@ -35,22 +35,62 @@
         </div>
         <q-btn class="lt-lg" dense flat round icon="menu" @click="toggleRightDrawer" />
       </q-toolbar>
-      <q-dialog v-model="prompt" persistent>
-        <q-card style="min-width: 350px">
-          <q-card-section>
-            <div class="text-h6">Your address</div>
-          </q-card-section>
+      <!-- 註冊對話框 -->
+      <q-form @submit="onSubmit" class="q-gutter-xs">
+        <q-dialog v-model="register" persistent>
 
-          <q-card-section class="q-pt-none">
-            <q-input dense v-model="address" autofocus @keyup.enter="prompt = false" />
-          </q-card-section>
-
-          <q-card-actions align="right" class="text-primary">
-            <q-btn flat label="Cancel" v-close-popup />
-            <q-btn flat label="Add address" v-close-popup />
-          </q-card-actions>
-        </q-card>
-      </q-dialog>
+          <q-stepper v-model="step" ref="stepper" color="primary" animated class="aaa" done-color="green">
+            <q-step :name="0" :title='t("register")' icon="regidter" :done="step > 0">
+              <q-card-section>
+                <div class="text-h4">{{ t("register") }}</div>
+                <div class="text-h6">{{ t("onlyStudent") }}</div>
+              </q-card-section>
+              <!-- <p class="text-h6">1.可選匿名 <br>2.評價依照課程名保存，更好查閱 <br>3.好的評價置頂 <br>4.評價越受歡迎，帳號分數越高</p> -->
+            </q-step>
+            <!--  -->
+            <q-step :name="1" title="驗證學校信箱" icon="email" :done="step > 1">
+              <q-card-section class="q-pt-none">
+                <q-input filled v-model="loginForm.schoolEMail" :label='t("schoolEMail")' lazy-rules
+                  :rules="[val => val && val.length > 0 || t('cantNull')]" />
+                <q-btn dense flat rounded :loading="mailSending" @click="sendMail" label="寄驗證信"> <template
+                    v-slot:loading>
+                    <q-spinner-radio />
+                  </template></q-btn>
+                <q-input filled v-model="loginForm.schoolEMailCode" :label='t("schoolEMailCode")' lazy-rules
+                  :rules=schoolEMailVal />
+                <div>
+                </div>
+              </q-card-section>
+            </q-step>
+            <!--  -->
+            <q-step :name="2" title="帳密" icon="settings" :done="step > 2">
+              <q-card-section class="q-pt-none">
+                <q-input filled v-model="registerForm.account" :label='t("account")' lazy-rules
+                  :rules="[val => val && val.length > 0 || t('cantNull')]" />
+                <q-input filled v-model="registerForm.password" :label='t("password")' :hint='t("pwdRule")' lazy-rules
+                  :rules="[val => val && val.length > 0 || t('cantNull')]" />
+                <q-input filled v-model="registerForm.passwordCheck" :label='t("pwdCheck")' lazy-rules
+                  :rules="[val => val && val.length > 0 || t('pwdCheckFail')]" />
+                <p class="gender">Gender:</p>
+                <div class="q-gutter-sm">
+                  <q-radio v-model="registerForm.gender" val=1 :label='t("Male")' />
+                  <q-radio v-model="registerForm.gender" val=2 :label='t("Female")' />
+                  <q-radio v-model="registerForm.gender" val=0 :label='t("Others")' />
+                </div>
+              </q-card-section>
+            </q-step>
+            <template v-slot:navigation>
+              <q-stepper-navigation>
+                <q-btn @click="$refs.stepper.next()" color="primary" :label="step === 4 ? 'Finish' : 'Continue'" />
+                <q-btn v-if="step > 0" flat color="primary" @click="$refs.stepper.previous()" label="Back"
+                  class="q-ml-sm" />
+                <q-btn v-if="step > 3" class="q-ml-sm" :label='t("register")' color="primary" flat
+                  @click="register = true" />
+              </q-stepper-navigation>
+            </template>
+          </q-stepper>
+        </q-dialog>
+      </q-form>
     </q-header>
     <q-drawer v-model='leftDrawerState' side="left" mini-to-overlay persistent bordered show-if-above :breakpoint="767"
       :width="300">
@@ -88,7 +128,9 @@ const localeOptions = [
 ]
 const { locale, t } = useI18n({ useScope: 'global' })
 locale.value = useQuasar().lang.getLocale()
-
+// 初始變數
+const register = ref(true)
+const step = ref(1)
 // 切換左右選單顯示
 const leftDrawerState = ref(false)
 const rightDrawerState = ref(false)
@@ -98,19 +140,43 @@ const toggleLeftDrawer = () => {
 const toggleRightDrawer = () => {
   rightDrawerState.value = !rightDrawerState.value
 }
-//
-const login = () => {
+// 登入/註冊驗證功能
+const schoolEMailVal = [
+  val => (val !== null && val !== '') || 'Please type your age',
+  val => (val > 0 && val < 100) || 'Please type a real age',
+  val => (val && val.length > 0) || t('schoolEMailCodeFail')
+]
+const loginForm = reactive({ account: '', password: '', keepLogin: false })
+const registerForm = reactive({ schoolEMail: '', schoolEMailCOde: '', account: '', password: '', passwordCheck: '', gender: '0' })
+
+// 寄email
+const mailSending = ref(false)
+const sendMail = () => {
 
 }
+const onSubmit = () => { }
 
-const loginForm = reactive({ password: '', account: '', keepLogin: false })
 </script>
 
-<style lang="sass" scoped>
+<style lang="sass" scoped >
 .q-header
   height: 48px
 .langSelect
   width: 100px
   & *
     color: white
+.gender
+  margin-bottom: 0
+  font-size: 0.8rem
+.q-stepper
+  width: 500px
+  height: 600px
+  position: relative
+  &::v-deep
+    .q-stepper__nav
+      position: absolute
+      bottom: 0
+    .q-field
+      margin-top: 0.6rem
+
 </style>
