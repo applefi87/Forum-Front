6536b23fc56f9b6ab690cc0b32759132592520d8
applefi87 <werrtyyui@yahoo.com.tw>
2022-08-10 20:27:27 +0800

    t

--- quasar.config.js
+++ quasar.config.js
@@ -78,7 +78,7 @@ module.exports = configure(function (/* ctx */) {
       // ignorePublicFolder: true,
       // minify: false,
       // polyfillModulePreload: true,
-      // distDir
+      distDir: 'dist',
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
