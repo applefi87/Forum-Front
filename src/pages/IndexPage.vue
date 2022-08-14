dae1d352424a7c70a45b19c81e595631f4bdc405
applefi87 <werrtyyui@yahoo.com.tw>
2022-08-14 10:23:32 +0800

    測試全部課程傳給客戶端vs 只回傳對應系所，要多久與客戶端性能是否足夠 目前看先過濾再回傳較快，畢竟我有Index同時子板塊應該是會慢慢看的

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,6 +3,7 @@
     <p>{{ text }}</p>
     <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px">
     <p>{{ t('message') }}</p>
+    <q-btn label="進入板塊" type="submit" color="primary" class="f-r" flat @click="testGetAllBoard" />
     <hr>
     <QuillEditor class="editor" toolbar="full" theme="bubble" v-model:content="text" contentType="html" />
     <!-- <button @click="send">send</button> -->
@@ -10,7 +11,7 @@
 </template>
 
 <script setup>
-
+import { api } from 'src/boot/axios'
 import { ref } from 'vue'
 import { QuillEditor } from '@vueup/vue-quill'
 // 造型
@@ -19,8 +20,13 @@ import '@vueup/vue-quill/dist/vue-quill.bubble.css'
 import { useI18n } from 'vue-i18n'
 const { t } = useI18n() // call `useI18n`, and spread `t` from  `useI18n` returning
 // return render context that included `t`
-
 const text = ref('')
+const testGetAllBoard = async () => {
+  const { data } = await api.post('/board/62f60dea0e28c1a18208dac8', { t: 5 })
+  console.log('ok')
+  const boards = data.result
+  console.log(boards.filter((b) => b.colData.c0 === '通識'))
+}
 </script>
 
 <style lang="sass">
