b8f750b7afd4be3abfaa4413039ad31ee2c90a26
School PC <werrtyyui@yahoo.com.tw>
2022-08-19 15:22:47 +0800

    介面產生完畢 處理post

--- src/components/publishArticle.vue
+++ src/components/publishArticle.vue
@@ -2,7 +2,11 @@
   <q-dialog v-model="publishArticleState" persistent v-if="categoryList.length > 0">
     <div>
       <q-form class="q-gutter-md" @submit.prevent="publish()">
+        <q-select v-if="uniqueList?.length > 0" outlined v-model="unique" :options="uniqueList" label="學期/班" dense
+          options-dense :behavior="$q.platform.is.ios === true ? 'dialog' : 'menu'" />
         <q-select outlined v-model="selectCat" :options="categoryCodeList" label="文章分類" dense options-dense
+          :behavior="$q.platform.is.ios === true ? 'dialog' : 'menu'" />
+        <q-select outlined v-model="privacy" :options="privacyList" :label="t('privacy')" dense options-dense
           :behavior="$q.platform.is.ios === true ? 'dialog' : 'menu'" />
         <table v-if="category">
           <!-- 評分 -->
@@ -29,11 +33,10 @@
             </td>
           </tr>
           <!-- 其他col -->
-          <!-- <tr v-if="category.cols?.length > 0"> -->
           <tr v-for="col in (category.cols?.length > 0 ? category.cols : [])" :key="col">
             <td>{{ t(col.n) }}</td>
             <td>
-              <q-input v-model="form['f' + selectCat.value].title" :rules="titleVal">
+              <q-input v-model="form['f' + selectCat.value].cols[col.n]">
               </q-input>
             </td>
           </tr>
@@ -41,11 +44,12 @@
           <tr>
             <td>{{ category.contentCol }}</td>
             <td>
-              <q-input v-model="form['f' + selectCat.value].content" :rules="titleVal">
+              <q-input v-model="form['f' + selectCat.value].content" :rules="contentVal">
               </q-input>
             </td>
           </tr>
         </table>
+        <q-btn :label="t('submit')" type="submit"></q-btn>
       </q-form>
     </div>
   </q-dialog>
@@ -54,10 +58,12 @@
 <script setup scoped>
 import { ref, reactive, inject, watch, computed } from 'vue'
 import { useI18n } from 'vue-i18n'
-import { useUserStore } from 'src/stores/user'
-import notify from '../utils/notify'
+import { apiAuth } from 'src/boot/axios'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+// import { useArticleStore } from 'src/stores/article'
 const { t } = useI18n()
-const users = useUserStore()
+// const articles = useArticleStore()
 // 初始變數
 const publishArticleState = inject('publishArticleState')
 // 版有unique資料
@@ -69,18 +75,35 @@ const article = inject('article')
 // 用for 建置 加上讀取規則自動產生
 // 如果換版>watch，把原本全清除for[key]，重新建
 const form = reactive({})
+const unique = ref('')
+// const uniqueList = reactive([])
 const selectCat = ref(null)
+const privacyList = computed(() => { return [{ label: t('showAll'), value: 1 }, { label: t('anonymous'), value: 0 }] })
+const privacy = ref(privacyList.value[0])
 // 先抓取文章分類+設個預設的
 const categoryList = reactive([])
 const categoryCodeList = computed(() =>
   article?.category?.map(c => { return { label: c.n, value: c.c } })
 )
-// 給vue元件用 不用重複算
+// ***************給vue元件用 不用重複算
 const category = computed(() => categoryList.find(c => {
   return c.c === selectCat?.value?.value
 }))
-// const formCategory = ref(categoryList?.value)
-// 使用者選的文章分類
+// 獨立選單建立
+const uniqueList = computed(() => {
+  console.log(board.uniqueData)
+  console.log(board?.uniqueData?.length)
+  if (board?.uniqueData?.length > 0) {
+    return board.uniqueData.map(u => {
+      return {
+        label: t('semester') + ':' + (u.c80 || t('none')) + ',' + t('time') + ':' + ((u.c85[0] && u.c85[0] !== '無') ? u.c85[0] : t('none')) + ',' + t('location') + ':' + ((u.c85[1] && u.c85[1] !== '無') ? u.c85[1] : t('none')), value: u._id
+      }
+    })
+  }
+  return []
+})
+
+// *********************
 
 // form 基礎object建立(依照article) 剩下vue會自動form.xxx 建立
 watch(article, () => {
@@ -90,10 +113,17 @@ watch(article, () => {
     categoryList.push(...article.category)
     // 對應加上form.fx
     categoryList?.forEach(f => {
-      form['f' + f.c] = { title: '', content: '' }
+      form['f' + f.c] = { title: 'wdaaaaaaaaaaaaa', content: 'wwwwwwwwwwwwwwww' }
       const formIn = form['f' + f.c]
       if (f.c === 1) formIn.score = 5
       if (f.tagActive) {
+        formIn.tags = []
+      }
+      if (f.cols?.length > 0) {
+        formIn.cols = {}
+        for (const col of f.cols) {
+          formIn.cols[col.n] = ''
+        }
         formIn.tags = []
       }
     })
@@ -103,11 +133,24 @@ watch(article, () => {
 })
 //
 const titleVal = [
-  val => (val && val.length >= 5 && val.length <= 30) || '長度需介於5~30字之間'
+  val => (val && val.length >= 5 && val.length <= 30) || '需5~30字之間'
+]
+const contentVal = [
+  val => (val && val.length >= 10 && val.length <= 500) || '需10~500字之間'
 ]
 // ****************發布****
 const publish = async () => {
-  notify()
+  if (route.params.id) {
+    try {
+      const submit = JSON.parse(JSON.stringify(form['f' + selectCat.value.value]))
+      submit.privacy = privacy.value
+      submit.category = selectCat.value.value
+      submit.uniqueId = unique.value.value
+      const { data } = await apiAuth.post('/article/create/' + (route.params.id ? route.params.id : '62fc99277f3adbe07e542a58'), form)
+    } catch (error) {
+      console.log(error.response.data)
+    }
+  }
 }
 
 </script>
