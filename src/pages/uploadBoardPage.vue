908bf3a9dfe67e2ec0915c2abf00d82a13710947
applefi87 <werrtyyui@yahoo.com.tw>
2022-08-21 12:37:07 +0800

    基本post

--- src/pages/uploadBoardPage.vue
+++ src/pages/uploadBoardPage.vue
@@ -18,11 +18,14 @@
 
 <script setup>
 import converter from 'json-2-csv'
+import { apiAuth } from 'src/boot/axios'
+import { useRoute, useRouter } from 'vue-router'
 import { ref, watch } from 'vue'
 import notify from 'src/utils/notify'
 import { useI18n } from 'vue-i18n'
-const { t } = useI18n() // call `useI18n`, and spread `t` from  `useI18n` returning
-// return render context that included `t`
+const { t } = useI18n()
+const route = useRoute()
+const router = useRouter()
 
 const file = ref(null)
 const onRejected = (file) => {
@@ -42,7 +45,7 @@ watch(file, () => {
   }
 })
 const out = ref('')
-const transform = () => {
+const transform = async () => {
   if (input.value) {
     converter.csv2json(
       input.value,
@@ -53,12 +56,14 @@ const transform = () => {
       },
       { delimiter: { wrap: '"', eol: '\r' } }
     )
+    const { data } = await apiAuth.post('/create/temp/' + route.params.id, { json: out.value, uniqueCol: uniqueCol.value })
   }
 }
 const uniqueColVal = [
   val => (val !== null && val !== '') || '必填 學年-期 ex: 111-1',
   val => (val.length === 5 && val.match(/^\d{3}-[1-3]$/)) || '規格錯誤 ex: 111-1'
 ]
+
 </script>
 
 <style lang="sass">
