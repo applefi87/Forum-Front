93ee2924886076c970648c0422ee321f99bd269a
applefi87 <werrtyyui@yahoo.com.tw>
2022-08-29 21:17:48 +0800

    測試chart....

--- src/boot/chartJS.js
+++ src/boot/chartJS.js
@@ -1,6 +1,5 @@
 import { boot } from 'quasar/wrappers'
 import VueApexCharts from 'vue3-apexcharts'
-
-export default boot(({ app }) => {
+boot(({ app }) => {
   app.use(VueApexCharts)
 })
--- src/components/chartInfo.vue
+++ src/components/chartInfo.vue
@@ -2,11 +2,11 @@
 <template>
   <q-card>
     <q-card-section>
-      <div v-if="p.form.titleCol" class="text-h6 text-weight-bold">{{  p.form.titleCol  }}: {{  p.form.title  }}</div>
-      <div v-if="p.form.score" class="text-weight-bold">{{  p.form.averageTitle  }}: {{  p.form.score  }}分</div>
+      <div v-if="p.form.titleCol" class="text-h6 text-weight-bold">{{ p.form.titleCol }}: {{ p.form.title }}</div>
+      <div v-if="p.form.score" class="text-weight-bold">{{ p.form.averageTitle }}: {{ p.form.score }}分</div>
     </q-card-section>
     <q-card-section v-if="p.form.datas" class="q-pt-none">
-      <div v-if="p.form.chartTitle" class="text-h6 text-weight-bold text-center">{{  p.form.chartTitle  }}</div>
+      <div v-if="p.form.chartTitle" class="text-h6 text-weight-bold text-center">{{ p.form.chartTitle }}</div>
       <apexchart type="pie" width="280" :options="options" :series="p.form.datas" class="q-pa-md">
       </apexchart>
     </q-card-section>
@@ -14,8 +14,12 @@
 </template>
 
 <script setup >
-
+import { createApp } from 'vue'
+import VueApexCharts from 'vue3-apexcharts'
 import ApexCharts from 'apexcharts'
+
+const app = createApp()
+app.use(VueApexCharts)
 const p = defineProps({
   form: Object
 })
